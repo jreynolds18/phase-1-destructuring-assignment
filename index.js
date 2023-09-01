d7b059f1d8d09ad9affffde2986832bd9d3167b1
Jesse Reynolds <jessereynolds18@gmail.com>
2023-09-01 12:27:13 -0500

    uses destructuring on strings, arrays, and objects

--- index.js
+++ index.js
@@ -28,21 +28,42 @@ const nestedMuppet = {
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+let animalsSeparate = farmAnimals.split(" ");
+let [moo, neigh, baa, oink, cluck] = animalsSeparate;
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
+let fourAnimalsLeft = [...animalsSeparate];
+let horseRanOff = fourAnimalsLeft.splice(1, 1);
+let [bessie, dolly, babe, little] = fourAnimalsLeft;
 
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
+let threeAnimalsLeft = [...fourAnimalsLeft];
+let chickenCooped = threeAnimalsLeft.pop();
+let [blackAndWhite, black, pink] = threeAnimalsLeft;
 
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
+let sixColors = [...colors];
+let noIndigo = sixColors.splice(5, 1);
+const [r, o, y, g, b, v] = sixColors;
 
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+let indg = noIndigo[0];
 
 // Objects
 
 // 7. Use destructuring to assign all variables using the keys as the variable names
+let {muppetName, color, song, job, partner} = {...muppet}
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+let kermitInfo = {...nestedMuppet}
+let {song2, song4, nestedJob, nestedPartner} = {
+  song2: kermitInfo.album.theMuppetMovie.song2,
+  song4: kermitInfo.album.theMuppetMovie.song4,
+  nestedJob: kermitInfo.nestedJob,
+  nestedPartner: kermitInfo.nestedPartner
+}
